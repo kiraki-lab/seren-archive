f8fd0b5ae89dead31c270f34855eb6da195a5361
kiraki-lab <luckykiraki@gmail.com>
2026-06-01 21:18:10 +0900

    Connect custom submit form to Apps Script web app

--- submit-config.js
+++ submit-config.js
@@ -1,4 +1,3 @@
 // 키라키 제보 폼 설정 파일
 // Apps Script 웹앱 배포 후 발급되는 URL을 아래 WEB_APP_URL에 넣으면 됩니다.
-// 예: const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycb.../exec';
-const WEB_APP_URL = '';
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbw2Gh7ZmsbKVbQPUMKZCiCn53iZVSHV00oImsiU_MeL4BWLCankojMke6zoIxzo8jAlzQ/exec';
