8fab9a926c54bc0946c8e8b763c5a9fbb281da45
kiraki-lab <luckykiraki@gmail.com>
2026-06-01 22:48:44 +0900

    Make submit web app URL globally available

--- submit-config.js
+++ submit-config.js
@@ -1,3 +1,4 @@
 // 키라키 제보 폼 설정 파일
-// Apps Script 웹앱 배포 후 발급되는 URL을 아래 WEB_APP_URL에 넣으면 됩니다.
-const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbw2Gh7ZmsbKVbQPUMKZCiCn53iZVSHV00oImsiU_MeL4BWLCankojMke6zoIxzo8jAlzQ/exec';
+// Apps Script Web App /exec URL
+window.KIRAKI_SUBMIT_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbw2Gh7ZmsbKVbQPUMKZCiCn53iZVSHV00oImsiU_MeL4BWLCankojMke6zoIxzo8jAlzQ/exec';
+const WEB_APP_URL = window.KIRAKI_SUBMIT_WEB_APP_URL;
