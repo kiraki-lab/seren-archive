f3d4c19047ebb731ecce9f7490e8245598b127da
kiraki-lab <luckykiraki@gmail.com>
2026-06-02 16:54:17 +0900

    Update submission web app URL

--- submit-config.js
+++ submit-config.js
@@ -1,4 +1,4 @@
 // 키라키 제보 폼 설정 파일
 // Apps Script Web App /exec URL
-window.KIRAKI_SUBMIT_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbw2Gh7ZmsbKVbQPUMKZCiCn53iZVSHV00oImsiU_MeL4BWLCankojMke6zoIxzo8jAlzQ/exec';
+window.KIRAKI_SUBMIT_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzxLs0yRIgTonfrhKpjK0gGBKiGtM-lpKsiKVxR6E2EGmFB0qAXVTBOBMxR732gYcpBeA/exec';
 const WEB_APP_URL = window.KIRAKI_SUBMIT_WEB_APP_URL;
